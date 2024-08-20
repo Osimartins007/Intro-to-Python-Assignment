c9a2e85e13b256e6d103519a2d7a597aa8cabddc
Osimartins007 <122929190+Osimartins007@users.noreply.github.com>
2024-08-20 03:43:41 -0500

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name and store it in a variable called "name"
+name = input("igbon osi martins ")
+
+# Ask the user for their age and store it in a variable called "age"
+age = input("33 years old ")
+
+# Ask the user for their location and store it in a variable called "location"
+location = input("Number six victor street off nosa edugie gra benin city ")
+
+# Print out a personalized message using the user's name, age, and location
+print(f"Hello {name}, you are {age} years old and live in {location}.")
